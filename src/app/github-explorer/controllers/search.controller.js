60a4aed34b1a8dac24f4482a7357055e5553185c
Dan Hutchinson <danielhutchinson5@gmail.com>
2016-10-05 20:12:01 +0100

    update gitignore for webstorm

--- src/app/github-explorer/controllers/search.controller.js
+++ src/app/github-explorer/controllers/search.controller.js
@@ -1,14 +1,12 @@
 var module = angular.module('githubExplorer');
 
 var searchController = function ($scope, $location) {
-	
 	$scope.query = '';
 
 	$scope.search = function () {
 		if($scope.query)
 			$location.path('/search/users').search('q', $scope.query);
 	};
-
 };
 
 module.controller('SearchController', ['$scope', '$location', searchController]);
